f6e6b83d65cdb74b58a1da0483fab44acd472fbb
Nipun Anupama <anupamaalahakoon66@gmail.com>
2025-03-09 09:32:34 +0530

    tailwind css Add

--- frontend/url-frontend/src/App.jsx
+++ frontend/url-frontend/src/App.jsx
@@ -1,15 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 import ShortenerForm from './components/ShortenerForm'
+import ShortenedLinks from './components/ShortenedLinks';
 
 function App() {
+  const [ShortenedUrls, setShortenedUrls] = useState([]);
 
-
+  const addshortenedurl = (shortUrl) => {
+    setShortenedUrls([...ShortenedUrls, shortUrl]);
+  };
   return (
-    <>
-      <ShortenerForm />
-    </>
+    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+      <div className='w-1/3'>
+        <ShortenerForm addshortenedurl={addshortenedurl} />
+        <ShortenedLinks shortenedUrls={ShortenedUrls} />
+        {/* <ToastContainer position="top-right" autoClose={3000} /> */}
 
-  )
+      </div>
+    </div>
+  );
 }
 
 
