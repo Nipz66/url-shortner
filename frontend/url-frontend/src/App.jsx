b6ca2c8af80a3f98dfae081d12fe6e35eb147d7f
Nipun Anupama <anupamaalahakoon66@gmail.com>
2025-03-09 09:09:59 +0530

    create shortendeLinks file

--- frontend/url-frontend/src/App.jsx
+++ frontend/url-frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
+import ShortenerForm from './components/ShortenerForm'
 
 function App() {
 
 
   return (
     <>
-
+      <ShortenerForm />
     </>
+
   )
 }
+
 
 export default App
--- frontend/url-frontend/src/components/ShortenedLinks.jsx
+++ frontend/url-frontend/src/components/ShortenedLinks.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-function ShortenedLinks() {
+const ShortenedLinks = ({ shortenedUrls }) => {
     return (
-        
-    )
-}
+        <div className='bg-white p-6 rounded-lg shadow-lg mt-4'>
+            <h2 className='text-xl font-bold mb-4'>Shortened Links</h2>
+            <ul>
+                {shortenedUrls.map((url, index) => (
+                    <li key={index} className='mb-2'>
+                        <a href={`${process.env.REACT_APP_API_URL}/${url}`} target='_blank' rel="noopener noreferrer" className="text-blue-500">
+                            {`${window.location.origin}/${url}`}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default ShortenedLinks
