a3702dd95433ec094a6e45b20330388ad3f5e2e2
Nipun Anupama <anupamaalahakoon66@gmail.com>
2025-03-11 23:43:34 +0530

    headlessui and heroicons npm install

--- frontend/url-frontend/src/App.jsx
+++ frontend/url-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import ShortenerForm from './components/ShortenerForm'
 import ShortenedLinks from './components/ShortenedLinks';
+import Header from './components/Head/Header';
 
 function App() {
   const [ShortenedUrls, setShortenedUrls] = useState([]);
@@ -10,12 +11,17 @@ function App() {
     setShortenedUrls([...ShortenedUrls, shortUrl]);
   };
   return (
-    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
-      <div className='w-1/3'>
-        <ShortenerForm addshortenedurl={addshortenedurl} />
-        <ShortenedLinks shortenedUrls={ShortenedUrls} />
-        {/* <ToastContainer position="top-right" autoClose={3000} /> */}
+    <div>
+      <Header />
 
+
+      <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+        <div className='w-1/3'>
+          <ShortenerForm addshortenedurl={addshortenedurl} />
+          <ShortenedLinks shortenedUrls={ShortenedUrls} />
+          {/* <ToastContainer position="top-right" autoClose={3000} /> */}
+
+        </div>
       </div>
     </div>
   );
