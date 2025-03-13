b8634e7871ef4fc23a22904d458c8aeacbc30121
Nipun Anupama <anupamaalahakoon66@gmail.com>
2025-03-13 23:53:56 +0530

    Icon Add

--- frontend/url-frontend/src/components/Body/Description.jsx
+++ frontend/url-frontend/src/components/Body/Description.jsx
@@ -1,29 +1,29 @@
-import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
+import { ShieldCheckIcon, LinkIcon, QrCodeIcon, CursorArrowRaysIcon } from '@heroicons/react/24/outline'
 
 const features = [
   {
-    name: 'Push to deploy',
+    name: 'Instant URL Shortening',
     description:
-      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
-    icon: CloudArrowUpIcon,
+      'Quickly shorten long URLs into compact, shareable links.',
+    icon: LinkIcon,
   },
   {
-    name: 'SSL certificates',
+    name: 'One-Click Copy',
     description:
-      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
-    icon: LockClosedIcon,
+      'Easily copy your shortened link with a single click',
+    icon: CursorArrowRaysIcon,
   },
   {
-    name: 'Simple queues',
+    name: 'Secure & Reliable',
     description:
-      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
-    icon: ArrowPathIcon,
+      'Ensures safe redirections without spam or malicious links.',
+    icon: ShieldCheckIcon,
   },
   {
-    name: 'Advanced security',
+    name: 'Generate a QR Code',
     description:
-      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-    icon: FingerPrintIcon,
+      'Instantly get a QR code for your shortened link.',
+    icon: QrCodeIcon,
   },
 ]
 
@@ -32,13 +32,11 @@ export default function Example() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
           <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl lg:text-balance">
-            Everything you need to deploy your app
+            A fast, easy, and free link shortener
           </p>
           <p className="mt-6 text-lg/8 text-gray-600">
-            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
-            pulvinar et feugiat blandit at. In mi viverra elit nunc.
+            Say goodbye to long, complex links and create short, user-friendly URLs in seconds. Whether you're sharing links on social media, emails, or messages, our free URL shortener ensures your links are clean, concise, and easy to remember.
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
