ffa87a64e13e23aafedd666144a017530c41c904
Nipun Anupama <anupamaalahakoon66@gmail.com>
2025-03-20 00:40:52 +0530

    About section

--- frontend/url-frontend/src/components/About/About.jsx
+++ frontend/url-frontend/src/components/About/About.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
 
+// const links = [
+//     { name: 'Open roles', href: '#' },
+//     { name: 'Internship program', href: '#' },
+//     { name: 'Our values', href: '#' },
+//     { name: 'Meet our leadership', href: '#' },
+// ]
+const stats = [
+    { name: 'Daily Active Users', value: '12,450 users+' },
+    { name: 'Monthly Active Users', value: '3385,600 users+' },
+    { name: 'Links Shortened Per Day', value: '8,560,000 links' },
+    { name: 'Total Links Shortened Since Launch', value: ' 5,420,000,000 links' },
+]
+
 function About() {
     return (
-        <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
+
+        <section className="relative isolate overflow-hidden bg-white px-6 py-14 sm:py-14 lg:px-16">
+
+            <div
+                aria-hidden="true"
+                className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            >
+                <div
+                    style={{
+                        clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                    className="relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                />
+            </div>
             <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,var(--color-indigo-100),white)] opacity-20" />
             <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white ring-1 shadow-xl shadow-indigo-600/10 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
             <div className="mx-auto max-w-2xl lg:max-w-4xl">
                 <img
                     alt=""
-                    src="https://tailwindcss.com/plus-assets/img/logos/workcation-logo-indigo-600.svg"
+                    src="https://yourwebsite.com/assets/logo.svg"
                     className="mx-auto h-12"
                 />
                 <figure className="mt-10">
                     <blockquote className="text-center text-xl/8 font-semibold text-gray-900 sm:text-2xl/9">
                         <p>
-                            “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
-                            molestiae. Numquam corrupti in laborum sed rerum et corporis.”
+                            "A LinkShortener, our mission is to simplify link sharing with speed, security, and smart analytics. Whether you're a business owner, marketer, or casual user, we make managing URLs effortless. Thank you for trusting us!"
                         </p>
                     </blockquote>
                     <figcaption className="mt-10">
@@ -34,8 +60,70 @@ function About() {
                     </figcaption>
                 </figure>
             </div>
+
+            <div className="relative isolate overflow-hidden bg-gray-900 mt-5 py-14 sm:py-32 px-6">
+                <img
+                    alt=""
+                    src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+                    className="absolute inset-0 -z-10 size-full object-cover object-right md:object-center "
+                />
+                <div
+                    aria-hidden="true"
+                    className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+                >
+                    <div
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                        className="aspect-1097/845 w-[68.5625rem] bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+                    />
+                </div>
+                <div
+                    aria-hidden="true"
+                    className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+                >
+                    <div
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                        className="aspect-1097/845 w-[68.5625rem] bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+                    />
+                </div>
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto max-w-2xl lg:mx-0">
+                        <h2 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">Link Shortening</h2>
+                        <p className="mt-8 text-lg font-medium text-pretty text-gray-300 sm:text-xl/8">
+                            Our platform is used worldwide by businesses, content creators, and individuals who need quick and reliable link shortening. Below, you’ll find insights into how users engage with our service across different regions.
+                        </p>
+                    </div>
+                    <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+                        {/* <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base/7 font-semibold text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+                            {links.map((link) => (
+                                <a key={link.name} href={link.href}>
+                                    {link.name} <span aria-hidden="true">&rarr;</span>
+                                </a>
+                            ))}
+                        </div> */}
+                        <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+                            {stats.map((stat) => (
+                                <div key={stat.name} className="flex flex-col-reverse gap-1">
+                                    <dt className="text-base/7 text-gray-300">{stat.name}</dt>
+                                    <dd className="text-4xl font-semibold tracking-tight text-white">{stat.value}</dd>
+                                </div>
+                            ))}
+                        </dl>
+                    </div>
+                </div>
+            </div>
+
         </section>
+
+
+
     )
 }
+
 
 export default About
