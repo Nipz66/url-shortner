95a5ee916a728b4146ad370400e7275ad66e61ae
Nipun Anupama <anupamaalahakoon66@gmail.com>
2025-03-22 00:00:00 +0530

    Login Page edit

--- frontend/url-frontend/src/components/Head/Header.jsx
+++ frontend/url-frontend/src/components/Head/Header.jsx
@@ -24,7 +24,7 @@ function Header() {
                 <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
                     <div className="flex lg:flex-1">
                         <a href="/Home" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
+                            <span className="sr-only">Link Shorter</span>
                             <img
                                 alt=""
                                 src="/image/1.jpeg"
@@ -52,7 +52,7 @@ function Header() {
                         ))}
                     </div>
                     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                        <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                        <a href="/login" className="text-sm/6 font-semibold text-gray-900">
                             Log in <span aria-hidden="true">&rarr;</span>
                         </a>
                     </div>
